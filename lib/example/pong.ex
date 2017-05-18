2a962b5d6d108b366a6863b76779ca79da565872
Nick van Eijk <nickvaneijk@MacBook-Pro-van-Nick.local>
2017-05-18 20:36:29 +0200

    Add second verb in example

--- lib/example/pong.ex
+++ lib/example/pong.ex
@@ -6,6 +6,7 @@ defmodule Example.Pong do
 
     {:ok, pid} = ZssService.get_instance config
     ZssService.Service.add_verb(pid, {"get", Examples.SampleHandler, :ping_me})
+    ZssService.Service.add_verb(pid, {"list", Examples.SampleHandler, :ping_me_more})
 
     ZssService.Service.run pid
   end
@@ -15,6 +16,13 @@ defmodule Examples.SampleHandler do
   def ping_me(payload, message) do
     {:ok, {
       %{ping: "PONG"},
+      Map.merge(message, %{status: "200"})
+     }}
+  end
+
+  def ping_me_more(payload, message) do
+    {:ok, {
+      [%{ping: "PONG"}, %{ping: "PANG"}],
       Map.merge(message, %{status: "200"})
      }}
   end
