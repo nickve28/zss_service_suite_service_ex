613e22ba775bd8b2e2faa2cbe7ec005894c6dc18
Nick van Eijk <nickvaneijk@MacBook-Pro-van-Nick.local>
2017-05-20 18:01:27 +0200

    Change extra_application mechanism and revert to older applications mechanism for backwards compat

--- mix.exs
+++ mix.exs
@@ -1,18 +1,24 @@
 defmodule ZssService.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [app: :zss_service,
-     version: "0.1.0",
+     version: @version,
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
   end
 
+  #Ideally we'd rely on application inference supported since 1.4
+  #But since we want to support <1.4, we rely on the older mechanism
+  @applications [:logger, :czmq, :uuid, :msgpax]
+
   def application do
     [
-      extra_applications: [:logger, :czmq, :uuid, :msgpax],
+      applications: @applications,
       mod: {ZssService, []}
     ]
   end
