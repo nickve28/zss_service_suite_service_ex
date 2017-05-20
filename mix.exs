e92e339d685ab86071597f38200e4b26dae99999
Nick van Eijk <nickve28@users.noreply.github.com>
2017-05-20 18:08:11 +0200

    Improvement/travis (#5)
    * add msgpax to running apps
    
    * Add travis yml
    
    * Change minimal elixir to 1.3
    
    * Set lrt flag to try and link clock lib on C level
    
    * Second attempt LDFLAGS option
    
    * Try on ubuntu trusty instead
    
    * Change extra_application mechanism and revert to older applications mechanism for backwards compat

--- mix.exs
+++ mix.exs
@@ -1,25 +1,31 @@
 defmodule ZssService.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [app: :zss_service,
-     version: "0.1.0",
-     elixir: "~> 1.4",
+     version: @version,
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
   end
 
+  #Ideally we'd rely on application inference supported since 1.4
+  #But since we want to support <1.4, we rely on the older mechanism
+  @applications [:logger, :czmq, :uuid, :msgpax]
+
   def application do
     [
-      extra_applications: [:logger, :czmq, :uuid],
+      applications: @applications,
       mod: {ZssService, []}
     ]
   end
 
   defp deps do
     [
-      {:czmq, github: "gar1t/erlang-czmq", compile: "./configure; make"},
+      {:czmq, github: "gar1t/erlang-czmq", compile: "LDFLAGS=-lrt ./configure; make"},
       {:msgpax, "~> 1.0"},
       {:uuid, "~> 1.1"}
     ]
