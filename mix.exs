e867bf1f0e21f4e8452a882a0f9aa0997c926110
nickve28 <nickvaneyk@gmail.com>
2017-05-25 10:08:34 +0200

    Add required hex publish information

--- mix.exs
+++ mix.exs
@@ -4,12 +4,17 @@ defmodule ZssService.Mixfile do
   @version "0.1.0"
 
   def project do
-    [app: :zss_service,
-     version: @version,
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :zss_service,
+      version: @version,
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/nickve28/zss_service_suite_service_ex"
+    ]
   end
 
   #Ideally we'd rely on application inference supported since 1.4
@@ -20,6 +25,21 @@ defmodule ZssService.Mixfile do
     [
       applications: @applications,
       mod: {ZssService, []}
+    ]
+  end
+
+  defp description do
+    """
+    A service worker to connect with the ZeroMQ ZSS Service Broker.
+    """
+  end
+
+  defp package do
+    [
+      name: :zss_service,
+      maintainers: ["Nick van Eijk"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/nickve28/zss_service_suite_service_ex"}
     ]
   end
 
