203a4ab3f2f2ab8c9e27450dda0d8d2810653b33
nickve28 <nickvaneyk@gmail.com>
2017-05-27 10:48:02 +0200

    Change time  test to ensure 1.3 DateTime support

--- test/zss_service/service/timer_test.exs
+++ test/zss_service/service/timer_test.exs
@@ -16,13 +16,18 @@ defmodule ZssService.Service.TimerTest do
     end
 
     test "#stop should measure the difference between start and end" do
-      {:ok, time, _} = DateTime.from_iso8601("2017-09-28T23:50:00.000Z")
+      #DateTime.from_iso8601 would have been preferable, but we need to ensure 1.3 support
+      {:ok, time} = 1_506_642_600_000_000_000 #"2017-09-28T23:50:00.000Z"
+      |> DateTime.from_unix(:nanoseconds)
+
       DateTimeStub.stub(:utc_now, time)
 
       start = Timer.start
 
       #add 1123 miliseconds
-      {:ok, end_time, _} = DateTime.from_iso8601("2017-09-28T23:50:01.123Z")
+      {:ok, end_time} = 1_506_642_601_123_000_000 #"2017-09-28T23:50:01.123Z"
+      |> DateTime.from_unix(:nanoseconds)
+
       DateTimeStub.stub(:utc_now, end_time)
 
       assert Timer.stop(start) === "1123.000"
