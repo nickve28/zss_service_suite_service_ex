12cfe0f2010439272a5d80fe9f09fe6452e9234d
nickve28 <nickvaneyk@gmail.com>
2017-07-16 11:47:19 +0200

    Return an error while constructing handlers if a handler is not found

--- lib/zss_service/configuration/config.ex
+++ lib/zss_service/configuration/config.ex
@@ -3,6 +3,8 @@ defmodule ZssService.Configuration.Config do
 
   alias ZssService.Configuration.{Config, Handler}
 
+  @fn_arity 2
+
   defstruct [
     heartbeat: 1000,
     broker: "tcp://127.0.0.1:7776",
@@ -18,12 +20,18 @@ defmodule ZssService.Configuration.Config do
   end
 
   def add_handler(%Config{handlers: handlers} = config, verb, {mod, fun}) do
-    handler_fn = fn payload, message ->
-      apply(mod, fun, [payload, message])
+    case function_exported?(mod, fun, @fn_arity) do
+      true ->
+        handler_fn = fn payload, message ->
+          apply(mod, fun, [payload, message])
+        end
+
+        upcase_verb = verb |> String.upcase
+
+        %Config{config | handlers: Map.put(handlers, upcase_verb, handler_fn)}
+      false ->
+        message = "The function #{mod} => #{fun} with arity #{@fn_arity} was not found"
+        {:error, message}
     end
-
-    upcase_verb = verb |> String.upcase
-
-    %Config{config | handlers: Map.put(handlers, upcase_verb, handler_fn)}
   end
 end
